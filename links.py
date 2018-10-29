3ec5ab57b91040f97bf58c78d43b81c5cde3152a
yan.huang <yan.huang@jrj.com.cn>
2018-10-29 14:30:54 +0800

    抓取视频

--- links.py
+++ links.py
@@ -1,6 +1,8 @@
 # -*- coding: UTF-8 -*- 
 import requests;
 import re
+import threading
+s1=threading.Semaphore(5)
 def geturl( url):
     c=requests.get(url)
     #print(c.content)
@@ -14,8 +16,38 @@ def geturl( url):
     if videoUrlO:
         #print(videoUrlO.group(1))
         vurl=videoUrlO.group(1)
-    return (title,vurl)        
+    return (title,vurl)
+def download(url,file):
+    c=requests.get(url)
+    f=open(file,'wb')
+    f.write(c.content) 
+    f.close()       
+        
 url="http://v.jrj.com.cn/2015-09-23/000000027276.shtml"
-a,b=geturl(url)
-print("title=",a)
-print("vurl=",b)    
+
+dir="c:\\download11\\"
+def downloadRun(url):
+    s1.acquire()
+    try:
+        a,b=geturl(u)
+        print("title=",a)
+        print("vurl=",b)  
+        file=dir+a+".mp4"
+        download(b,file)
+    except IOError as e:
+        print(e)
+    s1.release()
+f=open("u.txt","r")
+list=f.readlines();
+print(list)
+threads=[]
+for u  in list:
+    try:
+       t= threading.Thread(target=downloadRun,args=(u,))
+       threads.append(t)
+       t.start()
+    except IOError as e:
+        print(e)
+for t in threads:
+    t.join()  
+print("下载完成")         
